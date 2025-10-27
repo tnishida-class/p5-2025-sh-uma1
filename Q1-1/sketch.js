a288269b9dc92c3a0b0ed160d82d90198bab499c
sh-uma1 <0905.syuumai@gmail.com>
2025-10-27 15:40:03 +0900

    Change section1-1

--- Q1-1/sketch.js
+++ Q1-1/sketch.js
@@ -1,11 +1,11 @@
 // 神戸市のマーク
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(200, 190);
   background(255);
 
   noFill();
   strokeWeight(25);
   strokeCap(SQUARE);
-  arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
+  arc(100 + 25, 100, 100, 90, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
   // BLANK[1] 空欄を埋めて神戸市のロゴマークを完成させよう
 }
