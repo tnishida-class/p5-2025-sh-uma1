c113ed0b3813c4a61d6255e444ac8548cd5bfa5f
sh-uma1 <0905.syuumai@gmail.com>
2025-11-02 13:19:48 +0900

    Q2-1,2-2

--- Q2-2/sketch.js
+++ Q2-2/sketch.js
@@ -5,7 +5,12 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] ヒント： rectのx座標は size * i, y座標は size * j
+     if((i+j)%2===0){
+      fill(255);
+     }else{
+      fill(160);
+     } 
+     rect(size*i,size*j,size,size);// BLANK[1] ヒント： rectのx座標は size * i, y座標は size * j
     }
   }
 }
