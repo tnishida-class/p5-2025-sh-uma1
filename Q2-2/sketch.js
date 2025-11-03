7c4c75579185a2f082c569236039ed70826a890c
sh-uma1 <0905.syuumai@gmail.com>
2025-11-03 15:03:21 +0900

    Q2-2
    マスの上に◯を置く

--- Q2-2/sketch.js
+++ Q2-2/sketch.js
@@ -3,14 +3,33 @@ function setup() {
   createCanvas(200, 200);
   const size = width / 8; // マスの一辺の長さ
   noStroke();
+  //マスを書く
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
      if((i+j)%2===0){
-      fill(255);
+      fill(255);//白いマス
      }else{
-      fill(160);
+      fill(160);//グレーマス
      } 
      rect(size*i,size*j,size,size);// BLANK[1] ヒント： rectのx座標は size * i, y座標は size * j
     }
   }
+//丸を置く
+for(let j = 0; j<3; j++){
+  for(let i=0; i<8; i++){
+    if((i+j)%2===1){
+    fill(220,0,0);
+    circle(size*i + size / 2,size*j + size / 2,size*0.9)//赤丸
+  }
+}
+}
+for(let j = 5; j<8; j++){
+  for(let i = 0; i<8; i++){
+    if((i+j)%2===1){
+      fill(0);
+      circle(size*i + size / 2,size*j + size / 2,size*0.9)
+    }
+  }
+}
+
 }
